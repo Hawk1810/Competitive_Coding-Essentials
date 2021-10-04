9f6988b751b155c521cfbae7d53cec754b7373df
sanju6862 <ishwarkumawat6862@gmail.com>
2021-10-04 19:49:55 +0530

    added ishu

---
+++ codeforces/ishu/asg1.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define RAFTAR ios::sync_with_stdio (NULL);
+#define INPUT  cin.tie (NULL);
+#define OUTOUT  cout.tie (0);
+#define prn(n) cout<<n<<"\n"
+#define prs(n) cout<<n<<" "
+#define pr(n) cout<<n
+#define foi(n) for(int i=0;i<n;i++)
+#define foj(a,n) for(int j=a;j<n;j++)
+#define l long 
+#define ll long long
+#define nl cout<<"\n"
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+typedef map<int,int> mpi;
+typedef multimap<int,int> mmpi;
+typedef vector<int> veci;
+typedef vector<ll> vecl;
+typedef vector<string> vecs;
+typedef vector<float> vecf;
+typedef vector<pair<int,int>> vecp;
+int mod=1000003;
+void solve();
+/*************************************************************************************************/
+int main ()
+{
+ RAFTAR INPUT OUTOUT
+    
+ //solve();
+ int a,b;
+ cin>>a>>b;
+prn(a+b);
+  return 0;
+}
